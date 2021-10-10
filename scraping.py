30ade104a9028e0ccf36741b73f63518cd82d37f
Maria Jackeline Cortes <86242690+JackieCortes@users.noreply.github.com>
2021-10-09 19:44:57 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -11,6 +11,7 @@ def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
     
+    #This line tells that we are using the mars_news() function
     news_title, news_paragraph = mars_news(browser)
     
     # Run all scraping functions and store results in a dictionary
@@ -19,9 +20,10 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-    }
-
+        "last_modified": dt.datetime.now(),
+        "hemispheres":hemisphere_image_urls
+        }
+       
     # Stop webdriver and return data
     browser.quit()
     return data
@@ -29,7 +31,6 @@ def scrape_all():
 
 def mars_news(browser):
     #Scrape Mars News
-
     # Visit the mars nasa news site
     #url = 'https://redplanetscience.com'
     url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
@@ -57,7 +58,6 @@ def mars_news(browser):
         #news_p
     except AttributeError:
         return None, None
-    
     return news_title, news_p
 
 
@@ -95,12 +95,8 @@ def featured_image(browser):
 def mars_facts():
     # Add try/except for error handling
     try:
-    
-        #Creation of new DataFrame from the HTML table. 
-        #The Pandas function read_html()searches for and returns a list of tables
-        # (HTML). By index of 0, Pandas pulls the 1st table it encounters, 
-        #or the 1st item in the list. Then, it turns the table into a DataFrame.
-        #df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        #Creation of new DataFrame from the HTML table.The Pandas function read_html()searches for and returns a list of tables
+        # (HTML). By index of 0, Pandas pulls the 1st table it encounters or the 1st item in the list. Then, it turns the table into a DataFrame.
         df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
     except BaseException:
         return None
@@ -108,14 +104,42 @@ def mars_facts():
     #we assign columns to the new DataFrame for additional clarity.
     df.columns=['Description', 'Mars', 'Earth']
     #By using the .set_index()we're turning the Description column into df's index
-    #inplace=True means the updated index remains in place.
-    #without having to reassign the DataFrame to a new variable.
+    #inplace=True means the updated index remains in place without having to reassign the DataFrame to a new variable.
     df.set_index('Description', inplace=True)
-    #df
-
-    #convert our DataFrame back into HTML, easily with Pandas
-    #return df.to_html()
+   
+    #convert our DataFrame back into HTML, easily with Pandas #return df.to_html()
     return df.to_html(classes="table table-striped")
+
+def hem():
+    # A. Use browser to visit the URL 
+    url1 = 'https://marshemispheres.com/'
+    browser.visit(url1)
+    # B.Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    # C. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    # D. Write code to retrieve the image urls and titles for each hemisphere.
+    results = img_soup.find_all('a', class_="itemLink product-item")
+    list_h = []
+    hemisphere_image_urls = []
+    for n in range(0,len(results)-1,2):
+        list_h.append(results[n])
+        link = results[n].get('href')
+        img_html = f'https://marshemispheres.com/{link}'
+        
+        browser.visit(img_html)
+        html = browser.html
+        img_soup2 = soup(html, 'html.parser')
+        h2 = img_soup2.find('h2',class_="title").text
+        revisar = img_soup2.find('li')
+        link2 = revisar.find('a').get('href')
+        img_html2 = f'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/{link2}'
+        hemisphere_image_urls.append(
+                        {"image_URL":img_html2,
+                        "title": h2}
+                    )
+    return hemisphere_image_urls
 
 #browser.quit()
 if __name__ == "__main__":
